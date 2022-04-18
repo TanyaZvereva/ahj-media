71023fa84ada50cb03f3df781ed85de1f357d27e
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-04-18 18:43:54 +0300

    first commit 18.04

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css'
+const input = document.querySelector("input")
+input.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        
+    }
+})
---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
